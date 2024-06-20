e3bdba1e59201750651a5aa09afc191d3f2e2605
Pedro Rossi <pedro.rsr@outlook.com>
2024-06-20 17:46:05 -0300

    DELETE Trail endpoint

--- server/controllers/admins.py
+++ server/controllers/admins.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, jsonify, request
 
 from models import Admin, Trail
-from services import AuthenticationService
+from services import AuthenticationService, DBService
 
 admin_controller = Blueprint('admins', __name__)
 
 
-@admin_controller.route('/trails', methods = ['GET'])
+@admin_controller.route('/trails', methods=['GET'])
 def get_all_trails():
     try:
         AuthenticationService.authenticate(request)
@@ -18,7 +18,7 @@ def get_all_trails():
     return jsonify(trails_dict), 200
 
 
-@admin_controller.route('/trail/<int:trail_id>', methods = ['GET'])
+@admin_controller.route('/trail/<int:trail_id>', methods=['GET'])
 def get_trails(trail_id: int):
     try:
         AuthenticationService.authenticate(request)
@@ -30,7 +30,22 @@ def get_trails(trail_id: int):
     return jsonify(trail.to_dict()), 200
 
 
-@admin_controller.route('/login', methods = ['POST'])
+@admin_controller.route('/trail/<int:trail_id>', methods=['DELETE'])
+def delete_trail(trail_id: int):
+    try:
+        AuthenticationService.authenticate(request)
+    except Exception as e:
+        return {'message': repr(e)}, 401
+
+    trail: Trail = Trail.query.filter_by(id=trail_id).one()
+
+    DBService.session.delete(trail)
+    DBService.session.commit()
+
+    return {}, 200
+
+
+@admin_controller.route('/login', methods=['POST'])
 def login():
     user = request.json.get("username")
     password = request.json.get("password")
