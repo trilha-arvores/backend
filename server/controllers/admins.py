e14d0568b284ad6981ae9c07d7c5ef4c5f8994de
Davi Fagundes <davifagundes753@gmail.com>
2024-06-22 15:28:23 -0300

    burro pra caralho eu

--- server/controllers/admins.py
+++ server/controllers/admins.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
+import datetime
 
-from models import Admin, Trail
+from models import Admin, Trail, TreeTrail
 from services import AuthenticationService, DBService, ObjectStorageService
 
 admin_controller = Blueprint('admins', __name__)
@@ -14,7 +15,18 @@ def get_all_trails():
         return {'message': repr(e)}, 401
 
     all_trails = Trail.query.all()
-    trails_dict = [trail.to_dict() for trail in all_trails]
+    trails_dict = []
+    for trail in all_trails:
+        aux_trail = trail.to_dict()
+        trees = {}
+        trees_in_trail: list[TreeTrail] = TreeTrail.query.filter_by(trail_id=aux_trail["id"]).all()
+        for tt in trees_in_trail:
+            trees[tt.trail_order] = tt.tree.to_dict()
+
+            trees[tt.trail_order]["distance"] = float(tt.distance) if tt.distance else None
+        aux_trail["trees"] = trees
+        trails_dict.append(aux_trail)
+
     return jsonify(trails_dict), 200
 
 
@@ -26,8 +38,60 @@ def get_trails(trail_id: int):
         return {'message': repr(e)}, 401
 
     trail: Trail = Trail.query.filter_by(id=trail_id).one()
+    aux_trail = trail.to_dict()
+    trees = {}
+    trees_in_trail: list[TreeTrail] = TreeTrail.query.filter_by(trail_id=aux_trail["id"]).all()
+    for tt in trees_in_trail:
+        trees[tt.trail_order] = tt.tree.to_dict()
 
-    return jsonify(trail.to_dict()), 200
+        trees[tt.trail_order]["distance"] = float(tt.distance) if tt.distance else None
+    aux_trail["trees"] = trees
+
+    return jsonify(aux_trail), 200
+
+
+@admin_controller.route('/create', methods=["POST"])
+def create_trail():
+    try:
+        AuthenticationService.authenticate(request)
+    except Exception as e:
+        return {'message': repr(e)}, 401
+
+    name = request.json.get("name")
+    n_trees = request.json.get("n_trees")
+    distance = request.json.get("distance")
+    photo = request.json.get("photo")
+
+    trail = Trail(
+        name=name,
+        n_trees=n_trees,
+        distance=distance,
+        active=True,
+        photo=photo,
+        created_at=datetime.datetime.now()
+    )
+    DBService.session.add(trail)
+    DBService.session.commit()
+
+    just_added: Trail = Trail.query.filter_by(name=name).one()
+    if just_added is None:
+        return "Trail not found", 404
+
+    tree_list = request.json.get("trees")
+    if tree_list is None:
+        return "Tree list not provided", 400
+
+    for index, value in enumerate(tree_list):
+        tree_trail = TreeTrail(
+            tree_id=value,
+            trail_id=just_added.id,
+            trail_order=index,
+            active=True,
+        )
+        DBService.session.add(tree_trail)
+
+    DBService.session.commit()
+    return "Created", 201
 
 
 @admin_controller.route('/trail/<int:trail_id>', methods=['DELETE'])
